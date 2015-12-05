6f4d8fb7798bae35f0a5b7f321257e91d1c3f7a6
Stuart Longland <me@vk4msl.id.au>
2015-12-05 21:05:47 +1000

    Add support for iCE40-HX8K Breakout Board
    The PCF file is incomplete, but covers the onboard LEDs, the UART and
    the oscillator input.

--- build.sh
+++ build.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
+
+# Parse some command line arguments
+while [ $# -gt 0 ]
+do
+    case "$1" in
+        -1k)    PCF=icestick.pcf
+                DEV=1k
+                shift
+                ;;
+        -8k)    PCF=ice40hx8k-bb.pcf
+                DEV=8k
+                shift
+                ;;
+        -S)     ICEPROG_ARGS=-S
+                shift
+                ;;
+        *)
+                break
+                ;;
+    esac
+done
+
 # Build with open source tools
 if [ -z "$1" ]
 then
-    echo Usage: build.sh main_name [other .v files]
+    echo Usage: build.sh [-1k] [-8k] [-S] main_name [other .v files]
     echo Example: ./build.sh demo library.v
     exit 1
 fi    
@@ -12,9 +34,8 @@ shift
 echo Using yosys to synthesize design
 yosys -p "synth_ice40 -blif $MAIN.blif" $MAIN.v $@
 echo Place and route with arachne-pnr
-arachne-pnr -d 1k -p icestick.pcf $MAIN.blif -o $MAIN.txt
+arachne-pnr -d ${DEV} -p ${PCF} $MAIN.blif -o $MAIN.txt
 echo Converting ASCII output to bitstream
 icepack $MAIN.txt $MAIN.bin
 echo Sending bitstream to device
-iceprog $MAIN.bin
-
+iceprog ${ICEPROG_ARGS} $MAIN.bin
